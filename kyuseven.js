38b63b7427d42599549803d1596d63790654fa3c
github username <meliajm@gmail.com>
2020-06-08 17:12:25 -0500

    objects, sorting, iterating over object

---
+++ kyuseven.js
@@ -0,0 +1,10 @@
+function myLanguages(results) {
+  let arr = []
+  const aSort = Object.keys(results).sort(function(a,b){return results[b]-results[a]})  
+  for (let [key, value] of Object.entries(results)) {
+      if (value>=60) {
+        arr.push(key)
+      }
+  }
+  return aSort.filter(value => -1 !== arr.indexOf(value))
+}
