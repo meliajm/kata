2b61a89be47ef6ff3b884b8c5dd0b75fe11914dc
github username <meliajm@gmail.com>
2020-06-10 09:53:27 -0500

    splitAndAdd recursion

--- kyufive.js
+++ kyufive.js
@@ -98,11 +98,11 @@ var encryptThis = function(text) {
     return result.join(' ')
 }
 
-// const splitAndAdd = (arr, n) => {
-//     if (n <= 0)
-//       return arr;
-//     const ln = arr.length;
-//     if ((ln % 2) === 1)
-//       arr.unshift(0);
-//     return splitAndAdd(arr.splice(0, Math.ceil(ln / 2)).map((a, i) => a + arr[i]), n-1);
-//     }
+const splitAndAdd = (arr, n) => {
+    if (n <= 0)
+      return arr;
+    const ln = arr.length;
+    if ((ln % 2) === 1)
+      arr.unshift(0);
+    return splitAndAdd(arr.splice(0, Math.ceil(ln / 2)).map((a, i) => a + arr[i]), n-1);
+    }
