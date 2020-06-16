21d374e4dafb3260206b6c3841f30953f70e0a01
github username <meliajm@gmail.com>
2020-06-16 13:12:48 -0500

    starting java

---
+++ codewars.java
@@ -0,0 +1,11 @@
+public class Number {
+
+  public boolean isEven(double n) {
+      // Your awesome code here!
+      if (n % 2==0) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+}
