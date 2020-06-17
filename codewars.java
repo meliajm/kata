6b74100420e38d71895559fc7db06258c389c8e7
github username <meliajm@gmail.com>
2020-06-17 11:24:43 -0500

    java conditional

--- codewars.java
+++ codewars.java
@@ -15,3 +15,17 @@ public class ToString {
   public static final String a =Integer.toString(123);
 
 }
+
+public class Kata {
+  
+  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+
+
+    if (distanceToPump/mpg<=fuelLeft) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+}
